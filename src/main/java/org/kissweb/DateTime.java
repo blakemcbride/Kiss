02c62200951ba79d368068246eafe3304aabce33
Blake McBride <blake@arahant.com>
2019-12-15 05:17:57 -0600

    Added ability to get current date/time with a specified format and timezone

--- src/main/java/org/kissweb/DateTime.java
+++ src/main/java/org/kissweb/DateTime.java
@@ -2,6 +2,7 @@ package org.kissweb;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /*
  * User: Blake McBride
@@ -24,4 +25,20 @@ public class DateTime {
         SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy h:mm a");
         return df.format(date);
     }
+
+    /**
+     * Returns a string representing the current date and/or time with a specified format
+     * and within a specified timezone.
+     *
+     * @param dateFormat
+     * @param timeZone
+     * @return
+     */
+    public static String currentDateTimeFormatted(String dateFormat, String timeZone) {
+        Date date = new Date();
+        SimpleDateFormat df = new SimpleDateFormat(dateFormat);
+        df.setTimeZone(TimeZone.getTimeZone(timeZone));
+        return df.format(date);
+    }
+
 }
