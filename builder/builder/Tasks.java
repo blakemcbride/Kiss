a4e8dc985c02285b16be3c53baceaf1ba161a7da
Blake McBride <blake@arahant.com>
2020-04-17 09:53:09 -0500

    Updates tomcat Force tomcat stop on exit

--- builder/builder/Tasks.java
+++ builder/builder/Tasks.java
@@ -26,7 +26,7 @@ public class Tasks {
     final String LIBS = "libs";
     final ForeignDependencies foreignLibs = buildForeignDependencies();
     final LocalDependencies localLibs = buildLocalDependencies();
-    final String tomcatTarFile = "apache-tomcat-9.0.33.tar.gz";
+    final String tomcatTarFile = "apache-tomcat-9.0.34.tar.gz";
     final String BUILDDIR = "build.work";
     final String explodedDir = BUILDDIR + "/" + "exploded";
 
@@ -80,7 +80,7 @@ public class Tasks {
 
     void setupTomcat() {
         if (!exists("tomcat/bin/startup.sh")) {
-            download(tomcatTarFile, ".", "https://apache.cs.utah.edu/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz");  
+            download(tomcatTarFile, ".", "https://apache.cs.utah.edu/tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34.tar.gz");  
             gunzip(tomcatTarFile, "tomcat", 1);
             //run("tar xf apache-tomcat-9.0.31.tar.gz --one-top-level=tomcat --strip-components=1");
         }
---
+++ src/main/java/org/kissweb/rest/StartupListener.java
@@ -0,0 +1,22 @@
+package org.kissweb.rest;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Author: Blake McBride
+ * Date: 4/17/20
+ */
+public class StartupListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("* * * CONTEXT INITIALIZED * * *");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("* * * CONTEXT DESTROYED * * *");
+	System.exit(0);
+    }
+}
