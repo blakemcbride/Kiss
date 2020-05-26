ccd6b578606fb4b1707a50318de2490434c841a1
Blake McBride <blake@arahant.com>
2020-05-26 09:19:50 -0500

    Simplified the queue manager

--- src/main/java/org/kissweb/rest/QueueManager.java
+++ src/main/java/org/kissweb/rest/QueueManager.java
@@ -1,17 +1,11 @@
 package org.kissweb.rest;
 
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
 import javax.servlet.AsyncContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
-import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * Author: Blake McBride
@@ -20,52 +14,14 @@ import java.util.concurrent.LinkedBlockingDeque;
  * This class processes the incoming REST event queue.
  */
 class QueueManager {
-    private static final Logger logger = Logger.getLogger(QueueManager.class);
-    private final BlockingDeque<Packet> queue;
     private final ExecutorService pool;
 
-    private static int maxQueueSize = 1;
-    private final static Object lock = new Object();
-
-
     QueueManager(int maxThreads) {
-        queue = new LinkedBlockingDeque<>();
         pool = Executors.newFixedThreadPool(maxThreads);
-        Dispatcher dispatcher = new Dispatcher();
-        Thread thread = new Thread(dispatcher);
-        thread.start();
     }
 
     void add(HttpServletRequest request, HttpServletResponse response, PrintWriter out) {
-        queue.addLast(new Packet(request, response, out));
-
-        // keep track of how large the queue is getting
-        synchronized (lock) {
-            int m = queue.size();
-            if (m > maxQueueSize) {
-                maxQueueSize = m;
-                Level lvl = logger.getLevel();
-                logger.setLevel(Level.ALL);
-                logger.info("Max queue size = " + maxQueueSize);
-                logger.setLevel(lvl);
-            }
-        }
-    }
-
-    private class Dispatcher implements Runnable {
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    Packet packet = queue.takeFirst();
-                    Runnable r = new ProcessServlet(packet);
-                    pool.execute(r);
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
-        }
+        pool.execute(new ProcessServlet(new Packet(request, response, out)));
     }
 
     static class Packet {
