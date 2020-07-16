8f902ea4196d7cf533bd83d9e2d1c745a076c572
Blake McBride <blake@arahant.com>
2020-07-16 13:38:32 -0500

    Added some comments

--- src/main/application/scripts/MyScript.groovy
+++ src/main/application/scripts/MyScript.groovy
@@ -3,6 +3,10 @@ package scripts
 /**
  * Author: Blake McBride
  * Date: 7/16/20
+ *
+ * Groovy scripts get compiled and cached so they are run at the same speed as compiled code.
+ * They are also auto-recompiled and reloaded when they change so that the system can be changed
+ * while it is running.
  */
 class MyScript {
 
