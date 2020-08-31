f86d604212a7e58ce4d7bae149836352c6b02459
Blake McBride <blake@arahant.com>
2020-08-31 18:45:10 -0500

    Correction to isChangeChar

--- src/main/webapp/kiss/Utils.js
+++ src/main/webapp/kiss/Utils.js
@@ -1067,7 +1067,7 @@ class Utils {
     static isChangeChar(event) {
         if (!event || !event.key)
             return false;
-        return event.key.length === 1 || event.key === 'Backspace' || event.key === 'Delete';
+        return !event.ctrlKey && (event.key.length === 1 || event.key === 'Backspace' || event.key === 'Delete');
     }
 
     /**
