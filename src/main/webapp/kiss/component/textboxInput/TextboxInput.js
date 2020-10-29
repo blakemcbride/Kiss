937e30b24b45a9e769ba73e1994d416b6e81632c
Blake McBride <blake@arahant.com>
2020-10-29 17:51:13 -0500

    Auto-add scroll bar to textbox-input

--- src/main/webapp/kiss/component/textboxInput/TextboxInput.js
+++ src/main/webapp/kiss/component/textboxInput/TextboxInput.js
@@ -21,7 +21,7 @@
         let disabled = false;
         let resetContent = false;
         let placeholder = null;
-        let nstyle = "background-color: white; border: 2px solid #d0d5d5; display: inline-block; padding: 5px; word-wrap: anywhere; ";
+        let nstyle = "background-color: white; border: 2px solid #d0d5d5; display: inline-block; padding: 5px; word-wrap: anywhere; overflow-y: auto; ";
         if (attr.style)
             nstyle += attr.style;
 
