965ab11cc512f848832f7eedaae7d692afdd0c94
Blake McBride <blake@arahant.com>
2022-02-04 09:04:59 -0600

    Crypto enhancements to support random salt

--- src/main/java/org/kissweb/Crypto.java
+++ src/main/java/org/kissweb/Crypto.java
@@ -4,8 +4,13 @@ import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Author: Blake McBride
@@ -13,10 +18,23 @@ import java.security.Key;
  *
  * This class provides the ability to encrypt/decrypt strings and byte arrays with very strong (AES) encryption.
  */
-public class Crypto {
+public final class Crypto {
 
     private static final String ALGORITHM = "AES";
     private static String defaultPassword;
+    private static final Cipher cipher;
+    private static final BASE64Decoder base64Decoder = new BASE64Decoder();
+    private final static BASE64Encoder base64Encoder = new BASE64Encoder();
+    private final static Random random = new Random();
+
+    static {
+        try {
+            cipher = Cipher.getInstance(ALGORITHM);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            //e.printStackTrace();
+            throw new Error(e);
+        }
+    }
 
     /**
      * Encrypt a string utilizing the passed in salt and password.
@@ -32,10 +50,9 @@ public class Crypto {
      */
     public static String encrypt(String salt, String password, String valueToEnc) throws Exception {
         final Key key = generateKey(salt, password);
-        final Cipher c = Cipher.getInstance(ALGORITHM);
-        c.init(Cipher.ENCRYPT_MODE, key);
-        final byte[] encValue = c.doFinal(valueToEnc.getBytes());
-        return new BASE64Encoder().encode(encValue);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        final byte[] encValue = cipher.doFinal(valueToEnc.getBytes());
+        return base64Encode(encValue);
     }
 
     /**
@@ -48,6 +65,42 @@ public class Crypto {
      */
     public static String encrypt(String salt, String valueToEnc) throws Exception {
         return encrypt(salt, defaultPassword, valueToEnc);
+    }
+
+    /**
+     * Encrypt using the default password and no salt.
+     *
+     * @param valueToEnc
+     * @return
+     * @throws Exception
+     */
+    public static String encrypt(String valueToEnc) throws Exception {
+        return encrypt(null, defaultPassword, valueToEnc);
+    }
+
+    /**
+     * Encrypt with a given password and random salt.
+     *
+     * @param password
+     * @param valueToEnc
+     * @return
+     * @throws Exception
+     */
+    public static String encryptWithRandomSalt(String password, String valueToEnc) throws Exception {
+        final String salt = createSalt();
+        return salt + encrypt(salt, password, valueToEnc);
+    }
+
+    /**
+     * Encrypt using the default password and random salt.
+     *
+     * @param valueToEnc
+     * @return
+     * @throws Exception
+     */
+    public static String encryptWithRandomSalt(String valueToEnc) throws Exception {
+        final String salt = createSalt();
+        return salt + encrypt(salt, defaultPassword, valueToEnc);
     }
 
     /**
@@ -64,9 +117,8 @@ public class Crypto {
      */
     public static byte [] encrypt(String salt, String password, byte [] valueToEnc) throws Exception {
         final Key key = generateKey(salt, password);
-        final Cipher c = Cipher.getInstance(ALGORITHM);
-        c.init(Cipher.ENCRYPT_MODE, key);
-        return c.doFinal(valueToEnc);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(valueToEnc);
     }
 
     /**
@@ -83,6 +135,44 @@ public class Crypto {
     }
 
     /**
+     * Encrypt with the default password and no salt.
+     *
+     * @param valueToEnc
+     * @return
+     * @throws Exception
+     */
+    public static byte [] encrypt(byte [] valueToEnc) throws Exception {
+        return encrypt(null, defaultPassword, valueToEnc);
+    }
+
+    /**
+     * Encrypt with the given password and random salt.
+     *
+     * @param password
+     * @param valueToEnc
+     * @return
+     * @throws Exception
+     */
+    public static byte [] encryptWithRandomSalt(String password, byte [] valueToEnc) throws Exception {
+        final String salt = createSalt();
+        final byte [] salta = salt.getBytes();
+        final byte [] val = Arrays.copyOf(salta, salta.length + valueToEnc.length);
+        System.arraycopy(valueToEnc, 0, val, salta.length, valueToEnc.length);
+        return encrypt(salt, password, val);
+    }
+
+    /**
+     * Encrypt with the default password and random salt.
+     *
+     * @param valueToEnc
+     * @return
+     * @throws Exception
+     */
+    public static byte [] encryptWithRandomSalt(byte [] valueToEnc) throws Exception {
+        return encryptWithRandomSalt(defaultPassword, valueToEnc);
+    }
+
+    /**
      * Decrypt a string utilizing the passed in salt and password.
      *
      * While <code>salt</code> and <code>password</code> can be any size, they are added together and only the first
@@ -96,10 +186,9 @@ public class Crypto {
      */
     public static String decrypt(String salt, String password, String encryptedValue) throws Exception {
         final Key key = generateKey(salt, password);
-        final Cipher c = Cipher.getInstance(ALGORITHM);
-        c.init(Cipher.DECRYPT_MODE, key);
-        final byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedValue);
-        final byte[] decValue = c.doFinal(decordedValue);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        final byte[] decordedValue = base64Decode(encryptedValue);
+        final byte[] decValue = cipher.doFinal(decordedValue);
         return new String(decValue);
     }
 
@@ -116,6 +205,31 @@ public class Crypto {
     }
 
     /**
+     * Decrypt a string that was encrypted with random salt using the provided password.
+     *
+     * @param password
+     * @param encryptedValue
+     * @return
+     * @throws Exception
+     */
+    public static String decryptWithRandomSalt(String password, String encryptedValue) throws Exception {
+        final String salt = encryptedValue.substring(0, 11);
+        encryptedValue = encryptedValue.substring(11);
+        return decrypt(salt, password, encryptedValue);
+    }
+
+    /**
+     * Decrypt a string that was encrypted with random salt using the default password.
+     *
+     * @param encryptedValue
+     * @return
+     * @throws Exception
+     */
+    public static String decryptWithRandomSalt(String encryptedValue) throws Exception {
+        return decryptWithRandomSalt(defaultPassword, encryptedValue);
+    }
+
+    /**
      * Decrypt a byte array utilizing the passed in salt and password.
      *
      * While <code>salt</code> and <code>password</code> can be any size, they are added together and only the first
@@ -129,9 +243,8 @@ public class Crypto {
      */
     public static byte [] decrypt(String salt, String password, byte [] encryptedValue) throws Exception {
         final Key key = generateKey(salt, password);
-        final Cipher c = Cipher.getInstance(ALGORITHM);
-        c.init(Cipher.DECRYPT_MODE, key);
-        return c.doFinal(encryptedValue);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(encryptedValue);
     }
 
     /**
@@ -168,7 +281,7 @@ public class Crypto {
      * @return
      * @throws Exception
      */
-    private static Key generateKey(String salt, String password) throws Exception {
+    private static Key generateKey(String salt, String password) {
         int i = 0;
         final int size = 32;
         final byte [] bytes = new byte[size];
@@ -195,19 +308,67 @@ public class Crypto {
         return new SecretKeySpec(bytes, ALGORITHM);
     }
 
+    private static byte[] longtoBytes(long data) {
+        return new byte[]{
+                (byte) ((data >> 56) & 0xff),
+                (byte) ((data >> 48) & 0xff),
+                (byte) ((data >> 40) & 0xff),
+                (byte) ((data >> 32) & 0xff),
+                (byte) ((data >> 24) & 0xff),
+                (byte) ((data >> 16) & 0xff),
+                (byte) ((data >> 8) & 0xff),
+                (byte) (data & 0xff),
+        };
+    }
+
+    /**
+     * Base64 encode without the trailing =
+     *
+     * @param ba
+     * @return
+     */
+    private static String base64Encode(byte [] ba) {
+        final String c = base64Encoder.encode(ba);
+        return c.substring(0, c.length()-1);
+    }
+
+    /**
+     * Base 64 decode adding the trailing =
+     *
+     * @param s
+     * @return
+     * @throws IOException
+     */
+    private static byte [] base64Decode(String s) throws IOException {
+        return base64Decoder.decodeBuffer(s + "=");
+    }
+
+    /**
+     * Create a random salt.
+     * It is always 11 bytes long.
+     */
+    private static String createSalt() {
+        final long rl = random.nextLong();
+        final byte [] ba = longtoBytes(rl);
+        return base64Encode(ba);
+    }
+
     public static void main(String [] args) throws Exception {
-        String unencrypted = "Now is the time for all good men to come to the aid of their party.";
+        final String unencrypted = "Now is the time for all good men to come to the aid of their party.";
         final String password = "ThisIsASecretKey";
-        String salt1 = "ab";
-        String salt2 = "vr";
-        String encrypted1 = encrypt(salt1, password, unencrypted);
-        String encrypted2 = encrypt(salt2, password, unencrypted);
-        String decrypted1 = decrypt(salt1, password, encrypted1);
-        String decrypted2 = decrypt(salt2, password, encrypted2);
-        System.out.println("unencrypted  = \"" + unencrypted + "\" (" + unencrypted.length() + ")");
-        System.out.println("encrypted1   = \"" + encrypted1 + "\" (" + encrypted1.length() + ")");
-        System.out.println("encrypted2   = \"" + encrypted2 + "\" (" + encrypted2.length() + ")");
-        System.out.println("decrypted1   = \"" + decrypted1 + "\" (" + decrypted1.length() + ")");
-        System.out.println("decrypted2   = \"" + decrypted2 + "\" (" + decrypted2.length() + ")");
+
+        /*
+          The following example is interesting because we are encrypting the same text twice with the same password
+          yet getting totally different encryption texts.  Yet they both decrypt fine!
+         */
+        String encrypted1 = encryptWithRandomSalt(password, unencrypted);
+        String encrypted2 = encryptWithRandomSalt(password, unencrypted);
+        String decrypted1 = decryptWithRandomSalt(password, encrypted1);
+        String decrypted2 = decryptWithRandomSalt(password, encrypted2);
+        System.out.println("\n" + unencrypted + " (" + unencrypted.length() + ")");
+        System.out.println(encrypted1 + " (" + encrypted1.length() + ")");
+        System.out.println(encrypted2 + " (" + encrypted2.length() + ")");
+        System.out.println(decrypted1 + " (" + decrypted1.length() + ")");
+        System.out.println(decrypted2 + " (" + decrypted2.length() + ")");
     }
 }
