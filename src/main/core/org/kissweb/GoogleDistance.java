5f548a6acd12030c01265a94e268840fa85733d0
Blake McBride <blake@mcbridemail.com>
2022-03-04 21:20:33 -0600

    Add ability to get distances and times through Google maps

---
+++ src/main/core/org/kissweb/GoogleDistance.java
@@ -0,0 +1,101 @@
+package org.kissweb;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.IOException;
+
+/**
+ * This class gets the distance in miles between two addresses through a Google service.
+ * It also gets the estimated travel time in minutes.
+ * A Google API is needed for this to work.  (It comes from Google.)
+ *
+ * Author: Blake McBride
+ * Date: 3/4/22
+ */
+public class GoogleDistance {
+
+    private final static String URL = "https://maps.googleapis.com/maps/api/distancematrix/json";
+    private static String API_KEY;  // from Google
+    private final JSONObject result;
+    private JSONObject elm0 = null;
+
+    /**
+     * This key must be set before you can find any distances or times.
+     * The key comes from Google.
+     *
+     * @param key
+     */
+    public static void setAPIKey(String key) {
+        API_KEY = key;
+    }
+
+    /**
+     * Performs the actual query through Google.
+     * You must set the API_KEY before creating this object.
+     *
+     * @param fromAddress
+     * @param toAddress
+     * @return the JSON object returned by Google.
+     * @throws IOException
+     */
+    public GoogleDistance(String fromAddress, String toAddress) throws IOException {
+        final URLBuilder url = new URLBuilder(URL);
+        url.addParameter("origins", fromAddress);
+        url.addParameter("destinations", toAddress);
+        url.addParameter("units", "imperial");
+        url.addParameter("key", API_KEY);
+        final String surl = url.build();
+
+        RestClient rc = new RestClient();
+        result = rc.jsonCall("GET", surl);
+    }
+
+    /**
+     * Returns the distance in miles between two addresses.
+     *
+     * Returns -1 on error.
+     */
+    public int miles() {
+        try {
+            if (elm0 == null) {
+                elm0 = getElm0();
+                if (elm0 == null)
+                    return -1;
+            }
+            JSONObject dis = elm0.getJSONObject("distance");
+            return (int) ((double) dis.getInt("value") / 1606.244);
+        } catch (Throwable ex) {
+            return -1;
+        }
+    }
+
+    /**
+     * Return the estimated travel time in minutes.
+     * -1 is returned on error.
+     */
+    public int minutes() {
+        try {
+            if (elm0 == null) {
+                elm0 = getElm0();
+                if (elm0 == null)
+                    return -1;
+            }
+            JSONObject dur = elm0.getJSONObject("duration");
+            return dur.getInt("value") / 60;
+        } catch (Throwable ex) {
+            return -1;
+        }
+    }
+
+    private JSONObject getElm0() {
+        final JSONArray rows = result.getJSONArray("rows");
+        if (rows.length() != 1)
+            return null;
+        final JSONObject row0 = rows.getJSONObject(0);
+        final JSONArray elements = row0.getJSONArray("elements");
+        if (elements.length() != 1)
+            return null;
+        return elements.getJSONObject(0);
+    }
+
+}
