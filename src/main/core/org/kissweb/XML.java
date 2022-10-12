0c29f97548edb0aab7a0ffcc3cf0144e92350177
Blake McBride <blake@mcbridemail.com>
2022-10-12 12:08:51 -0500

    Beginnings of an XML class

---
+++ src/main/core/org/kissweb/XML.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2022 STACK360 LLC
+ * All rights reserved.
+ */
+
+package org.kissweb;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.transform.TransformerException;
+
+/**
+ * Author: Blake McBride
+ * Date: 9/18/22
+ */
+public class XML {
+
+
+    /**
+     * Format an XML Document into a pretty formatted String.
+     *
+     * @param document
+     * @return
+     */
+    public static String format(final Document document) {
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            StreamResult result = new StreamResult(new StringWriter());
+            DOMSource source = new DOMSource(document);
+            transformer.transform(source, result);
+            return result.getWriter().toString();
+        } catch (IllegalArgumentException | TransformerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Format an XML String into a pretty formatted String.
+     *
+     * @param unformattedXml
+     * @return
+     */
+    public static String format(String unformattedXml) {
+        return format(parse(unformattedXml));
+    }
+
+    /**
+     * Parse an XML string into an XML Document.
+     *
+     * @param in
+     * @return
+     */
+    public static Document parse(String in) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(in));
+            return db.parse(is);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
