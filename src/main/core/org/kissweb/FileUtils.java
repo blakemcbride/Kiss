1ac2b73ca96ed74bcb7decc24f3f6936ce18bd00
Blake McBride <blake@mcbridemail.com>
2022-11-09 18:03:39 -0600

    Merge KissFile and FileUtils classes

--- src/main/core/org/kissweb/FileUtils.java
+++ src/main/core/org/kissweb/FileUtils.java
@@ -4,6 +4,10 @@ import org.kissweb.restServer.MainServlet;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * Utilities to deal with files on the back-end.  These are usually PDF or CSV files that
@@ -85,4 +89,36 @@ public class FileUtils {
         return getHTTPPath(new File(fn));
     }
 
+    /**
+     * Write String to file fileName.  File is created if it doesn't exist and truncated otherwise.
+     *
+     * @param fileName
+     * @param str
+     * @throws IOException
+     */
+    public static void writeString(String fileName, String str) throws IOException {
+        Files.write(Paths.get(fileName), str.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    /**
+     * Write String to file fileName.  File is created if it doesn't exist and appended otherwise.
+     *
+     * @param fileName
+     * @param str
+     * @throws IOException
+     */
+    public static void appendString(String fileName, String str) throws IOException {
+        Files.write(Paths.get(fileName), str.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND);
+    }
+
+    /**
+     * Read the entire contents of a file into a String.
+     *
+     * @param fileName
+     * @return
+     * @throws IOException
+     */
+    public static String readFile(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
+    }
 }
