c7994bd682d5acc7475d2ce4314d83289c580113
Blake McBride <blake@mcbridemail.com>
2023-03-27 21:02:02 -0500

    Windows correction

--- bld.cmd
+++ bld.cmd
@@ -1,2 +1,2 @@
 @echo off
-java -cp "build.work\exploded\WEB-INF\classes:libs\commons-compress-1.20.jar" org.kissweb.builder.BuildUtils %*
+java -cp "build.work\exploded\WEB-INF\classes;libs\commons-compress-1.20.jar" org.kissweb.builder.BuildUtils %*
