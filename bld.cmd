148251888913413b89a8d9431df2106f79f68b0f
Blake McBride <blake@mcbridemail.com>
2023-08-06 13:41:51 -0500

    Update bld.cmd

--- bld.cmd
+++ bld.cmd
@@ -12,24 +12,30 @@ set "CLASS_FILE_2=build.work\exploded\WEB-INF\classes\org\kissweb\builder\BuildU
 robocopy "%~dp1." "%~dp2." "%~nx1" /L /is /njh /njs /ndl /nc /ns /ts | findstr /b /c:"%~t1"
 exit /b
 
-call :is_newer "%JAVA_FILE_1%" "%CLASS_FILE_1%"
-set "result1=%errorlevel%"
-call :is_newer "%JAVA_FILE_2%" "%CLASS_FILE_2%"
-set "result2=%errorlevel%"
+:: If build.work directory doesn't exist, consider the .java files to be "newer"
+if not exist build.work (
+    set "result1=1"
+    set "result2=1"
+) else (
+    call :is_newer "%JAVA_FILE_1%" "%CLASS_FILE_1%"
+    set "result1=%errorlevel%"
+    call :is_newer "%JAVA_FILE_2%" "%CLASS_FILE_2%"
+    set "result2=%errorlevel%"
+)
 
 :: If either .java file is newer, compile the .java files to .class files
 if "%result1%"=="1" (
-  echo Building the builder
-  if not exist build.work\exploded\WEB-INF\classes (
-    mkdir build.work\exploded\WEB-INF\classes
-  )
-  javac -cp libs\commons-compress-1.20.jar "%JAVA_FILE_1%" "%JAVA_FILE_2%" -d build.work\exploded\WEB-INF\classes
+    echo Building the builder
+    if not exist build.work\exploded\WEB-INF\classes (
+        mkdir build.work\exploded\WEB-INF\classes
+    )
+    javac -cp libs\commons-compress-1.20.jar "%JAVA_FILE_1%" "%JAVA_FILE_2%" -d build.work\exploded\WEB-INF\classes
 ) else if "%result2%"=="1" (
-  echo Building the builder
-  if not exist build.work\exploded\WEB-INF\classes (
-    mkdir build.work\exploded\WEB-INF\classes
-  )
-  javac -cp libs\commons-compress-1.20.jar "%JAVA_FILE_1%" "%JAVA_FILE_2%" -d build.work\exploded\WEB-INF\classes
+    echo Building the builder
+    if not exist build.work\exploded\WEB-INF\classes (
+        mkdir build.work\exploded\WEB-INF\classes
+    )
+    javac -cp libs\commons-compress-1.20.jar "%JAVA_FILE_1%" "%JAVA_FILE_2%" -d build.work\exploded\WEB-INF\classes
 )
 
 :: Run the BuildUtils class
