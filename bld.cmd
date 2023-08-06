efa109741b6cf89313ce09c7cf3cfd22f6c4d5f6
Blake McBride <blake@mcbridemail.com>
2023-08-06 13:48:09 -0500

    Another bld.cmd correction

--- bld.cmd
+++ bld.cmd
@@ -7,30 +7,23 @@ set "CLASS_FILE_1=build.work\exploded\WEB-INF\classes\org\kissweb\builder\Tasks.
 set "JAVA_FILE_2=src\main\core\org\kissweb\builder\BuildUtils.java"
 set "CLASS_FILE_2=build.work\exploded\WEB-INF\classes\org\kissweb\builder\BuildUtils.class"
 
-:: Function to compare file timestamps
-:is_newer
-robocopy "%~dp1." "%~dp2." "%~nx1" /L /is /njh /njs /ndl /nc /ns /ts | findstr /b /c:"%~t1"
-exit /b
-
-:: If build.work directory doesn't exist, consider the .java files to be "newer"
-if not exist build.work (
-    set "result1=1"
-    set "result2=1"
+:: Set a flag if any source file is newer than its corresponding class file or the class file doesn't exist
+set "buildRequired=0"
+if not exist "%CLASS_FILE_1%" (
+    set "buildRequired=1"
 ) else (
-    call :is_newer "%JAVA_FILE_1%" "%CLASS_FILE_1%"
-    set "result1=%errorlevel%"
-    call :is_newer "%JAVA_FILE_2%" "%CLASS_FILE_2%"
-    set "result2=%errorlevel%"
+    for /F "delims=" %%a in ('dir /B /O:D "%JAVA_FILE_1%" "%CLASS_FILE_1%"') do set "latest=%%a"
+    if "%latest%"=="%~nx1" set "buildRequired=1"
+)
+if not exist "%CLASS_FILE_2%" (
+    set "buildRequired=1"
+) else (
+    for /F "delims=" %%a in ('dir /B /O:D "%JAVA_FILE_2%" "%CLASS_FILE_2%"') do set "latest=%%a"
+    if "%latest%"=="%~nx2" set "buildRequired=1"
 )
 
 :: If either .java file is newer, compile the .java files to .class files
-if "%result1%"=="1" (
-    echo Building the builder
-    if not exist build.work\exploded\WEB-INF\classes (
-        mkdir build.work\exploded\WEB-INF\classes
-    )
-    javac -cp libs\commons-compress-1.20.jar "%JAVA_FILE_1%" "%JAVA_FILE_2%" -d build.work\exploded\WEB-INF\classes
-) else if "%result2%"=="1" (
+if "%buildRequired%"=="1" (
     echo Building the builder
     if not exist build.work\exploded\WEB-INF\classes (
         mkdir build.work\exploded\WEB-INF\classes
