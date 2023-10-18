673c26f96a5e380f6833feb0d8a0584bdd537dcd
Blake McBride <blake@mcbridemail.com>
2023-10-18 11:55:32 -0500

    Add ability to create a back-end-only project

---
+++ remove-frontend.cmd
@@ -0,0 +1,17 @@
+@echo off
+REM I've found that I often use multiple front-ends with a single back-end.
+REM So, this single back-end has no front-end code. This script
+REM removes the front-end portion of the system to give me a convenient
+REM starting point for a back-end-only project.
+
+if "%~1" neq "remove" (
+    echo.
+    echo This script removes the front-end portion of Kiss
+    echo.
+    echo Usage: %~0 remove
+    echo.
+    exit /b
+)
+
+rmdir /s /q .git* src\main\frontend remove-backend* make-front-end SimpleWebServer.jar upgrade-kiss* *.md
+rmdir /s /q expand LICENSE.txt list-both serv* shrink make-frontend
