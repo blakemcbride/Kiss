7dbeb0a141599f763890b44d604e6baf1ffdf4b3
Blake McBride <blake@mcbridemail.com>
2023-10-18 19:51:34 -0500

    Added make-frontend for Windows

---
+++ make-frontend.cmd
@@ -0,0 +1,19 @@
+@echo off
+REM This script is used to package up a front-end-only system
+
+SET FILE=frontend.war
+
+REM Delete the existing frontend.war file if it exists
+if exist %FILE% del %FILE%
+
+REM Change directory to src/main/frontend
+cd src\main\frontend
+
+REM Create a new frontend.war archive in the parent directory
+jar cvf ..\..\..\%FILE% *
+
+REM Return to the original directory
+cd ..\..\..
+
+echo Frontend package (%FILE%) created successfully.
+
