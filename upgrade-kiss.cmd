d2837555e11bcefedd6fc6574647332bc9b86c14
Blake McBride <blake@mcbridemail.com>
2025-05-10 14:19:15 -0500

    Update update scripts

--- upgrade-kiss.cmd
+++ upgrade-kiss.cmd
@@ -1,9 +1,7 @@
 @echo off
 REM Upgrade a Kiss-based application with the latest version of Kiss.
 
-IF NOT "%~1"=="" (
-    SET "TARGET=%~1"
-) ELSE (
+if "%~1"=="" (
     echo.
     echo * * * This is to be run from the virgin Kiss clone and not your application project directory. * * *
     echo.
@@ -13,28 +11,31 @@ IF NOT "%~1"=="" (
     exit /b 1
 )
 
-IF NOT EXIST "%TARGET%\" (
-    echo Directory "%TARGET%" does not exist.
+if not exist "%~1\" (
+    echo Directory %~1 does not exist.
     exit /b 1
 )
 
 call bld realclean
-pushd "%TARGET%"
+pushd "%~1"
 call bld realclean
 popd
 
-robocopy libs "%TARGET%\libs" /E /NFL /NDL /NJH /NJS /NC /NS
-robocopy src\main\core "%TARGET%\src\main\core" /MIR /NFL /NDL /NJH /NJS /NC /NS
-robocopy src\main\frontend\kiss "%TARGET%\src\main\frontend\kiss" /MIR /NFL /NDL /NJH /NJS /NC /NS
+robocopy libs "%~1\libs" /E
+robocopy src\main\core "%~1\src\main\core" /E /MIR
+robocopy src\main\frontend\kiss "%~1\src\main\frontend\kiss" /E /MIR
 
-rd /s /q "%TARGET%\src\main\frontend\WEB-INF"
-rd /s /q "%TARGET%\src\main\frontend\META-INF"
+rmdir /S /Q "%~1\src\main\frontend\WEB-INF"
+rmdir /S /Q "%~1\src\main\frontend\META-INF"
 
-robocopy src\main\frontend\lib "%TARGET%\src\main\frontend\lib" /E /NFL /NDL /NJH /NJS /NC /NS
+robocopy src\main\frontend\lib "%~1\src\main\frontend\lib" /E
+robocopy . "%~1" bld
+robocopy . "%~1" bld.cmd
+robocopy . "%~1" SimpleWebServer.jar
+robocopy . "%~1" serve
+robocopy . "%~1" serve.cmd
+robocopy . "%~1" view-log
 
-copy /Y bld "%TARGET%\bld" >nul
-copy /Y bld.cmd "%TARGET%\bld.cmd" >nul
-copy /Y SimpleWebServer.jar "%TARGET%\SimpleWebServer.jar" >nul
-copy /Y serve "%TARGET%\serve" >nul
-copy /Y serve.cmd "%TARGET%\serve.cmd" >nul
-copy /Y view-log "%TARGET%\view-log" >nul
+if not exist "%~1\src\main\precompiled\Tasks.java" (
+    copy src\main\precompiled\Tasks.java "%~1\src\main\precompiled\Tasks.java"
+)
